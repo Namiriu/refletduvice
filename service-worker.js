ca1a6c95366c469512ba84c048f3cef15be105cc
Namiriu <51415441+Namiriu@users.noreply.github.com>
2025-10-05 22:06:03 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Basic offline cache; requires HTTPS or localhost to activate
-const CACHE = 'instability-v3';
+const CACHE = 'instability-v4';
 const ASSETS = [
   './',
   './index.html',
@@ -10,6 +10,21 @@ const ASSETS = [
   './audio/ambient_loop.wav',
   './audio/groan.wav'
   './img/bg.webp',
+  './img/gauge_fill.png',
+  './img/gauge_track.png',
+  './img/btn_plus1.png',
+  './img/btn_plus2.png',
+  './img/btn_plus3.png',
+  './img/btn_plus5.png',
+  './img/btn_plus10.png',
+  './img/btn_plus20.png',
+  './img/btn_moins1.png',
+  './img/btn_moins2.png',
+  './img/btn_moins3.png',
+  './img/btn_moins5.png',
+  './img/btn_moins10.png',
+  './img/btn_moins20.png',
+
 ];
 self.addEventListener('install', (e)=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
