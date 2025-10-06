0a558f51cf63e838b1d46cbc06378671a619e950
Namiriu <51415441+Namiriu@users.noreply.github.com>
2025-10-06 11:45:48 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -23,7 +23,17 @@ const ASSETS = [
   './img/btn_moins3.png',
   './img/btn_moins5.png',
   './img/btn_moins10.png',
-  './img/btn_moins20.png'
+  './img/btn_moins20.png',
+  './audio/sounds/creepy_crow_caw.mp3',
+  './audio/sounds/creepy_ghost_whisper.mp3',
+  './audio/sounds/creepy_laugh.mp3',
+  './audio/sounds/creepy_wind.mp3',
+  './audio/sounds/door_slam_angrily.mp3',
+  './audio/sounds/footsteps_on_wooden_floor.mp3',
+  './audio/sounds/forest_whisper.mp3',
+  './audio/sounds/scratching_metal.mp3',
+  './audio/sounds/whisper_voices.mp3',
+  './audio/sounds/wood_creak_single.mp3'
 ];
 self.addEventListener('install', (e)=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
